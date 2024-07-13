dac21f3774f383714e9109b9f821d5ca6a7f79ea
ClintTheEngineer <81938100+ClintTheEngineer@users.noreply.github.com>
2024-07-13 17:47:07 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const corsOptions = {
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Origin', 'https://cander-dashboard.netlify.app'); 
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
